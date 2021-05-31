1f9432f15023f9df192086849b90b11d2cfde304
iarmankhan <work.armankhan@gmail.com>
2021-05-31 19:54:05 +0530

    :sparkles: cart item UI

--- src/components/Cart/CartItem.tsx
+++ src/components/Cart/CartItem.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
-import { Box } from "../../theme/Theme";
-import { palette } from "../../theme";
+import { Box, Text } from "../../theme/Theme";
 
 const CartItem: React.FC = () => {
   return (
@@ -10,8 +9,33 @@ const CartItem: React.FC = () => {
         width={120}
         height={120}
         borderRadius="m"
-        style={{ backgroundColor: palette.yellow }}
+        style={{ backgroundColor: "#bfeaf5" }}
       />
+      <Box padding="m" flex={1} justifyContent="center">
+        <Text>Size M, L</Text>
+        <Text variant="title3" marginBottom="s">
+          Short Sleeve Organic Top
+        </Text>
+        <Text variant="title3" color="primary">
+          $29.99
+        </Text>
+      </Box>
+      <Box justifyContent="center">
+        <Box
+          backgroundColor="secondary"
+          style={{
+            width: 24,
+            height: 24,
+            borderRadius: 12,
+          }}
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Text variant="header" color="background">
+            x2
+          </Text>
+        </Box>
+      </Box>
     </Box>
   );
 };
--- src/screens/Home/Cart.tsx
+++ src/screens/Home/Cart.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Dimensions, ScrollView, StyleSheet } from "react-native";
+import { ScrollView, StyleSheet } from "react-native";
 import Svg, { Path } from "react-native-svg";
 
 import CartContainer from "../../components/Cart/CartContainer";
@@ -9,7 +9,6 @@ import { Box, Text } from "../../theme/Theme";
 import CartItem from "../../components/Cart/CartItem";
 import { aspectRatio, useTheme } from "../../theme";
 
-const { width } = Dimensions.get("window");
 const height = 100 * aspectRatio;
 
 const d = "M 0 0 A 50 50 0 0 0 50 50 H 325 A 50 50 0 0 1 375 100 V 0 Z";
@@ -25,26 +24,33 @@ const Cart: React.FC<HomeNavigationProps<"Cart">> = ({ navigation }) => {
           title="Shopping Cart"
         />
       </Box>
-      <Box width={width} height={height}>
-        <Svg style={StyleSheet.absoluteFill} viewBox="0 0 375 100">
-          <Path d={d} fill={theme.colors.primary} />
-        </Svg>
-        <Text variant="title2" color="background" textAlign="center">
-          3 Items Added
-        </Text>
+      <Box flex={1}>
+        <ScrollView
+          style={{
+            borderBottomLeftRadius: theme.borderRadii.xl,
+            borderBottomRightRadius: theme.borderRadii.xl,
+          }}
+          contentContainerStyle={{
+            paddingVertical: 50 * aspectRatio,
+          }}
+          showsVerticalScrollIndicator={false}
+        >
+          <CartItem />
+          <CartItem />
+          <CartItem />
+          <CartItem />
+        </ScrollView>
+        <Box
+          style={{ position: "absolute", top: 0, left: 0, right: 0, height }}
+        >
+          <Svg style={StyleSheet.absoluteFill} viewBox="0 0 375 100">
+            <Path d={d} fill={theme.colors.primary} />
+          </Svg>
+          <Text variant="title2" color="background" textAlign="center">
+            3 Items Added
+          </Text>
+        </Box>
       </Box>
-      <ScrollView
-        style={{
-          borderBottomLeftRadius: theme.borderRadii.xl,
-          borderBottomRightRadius: theme.borderRadii.xl,
-        }}
-        showsVerticalScrollIndicator={false}
-      >
-        <CartItem />
-        <CartItem />
-        <CartItem />
-        <CartItem />
-      </ScrollView>
     </CartContainer>
   );
 };
