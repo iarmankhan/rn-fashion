34c8883d174cd3b81a64dc6c5be2e279b8a170dd
iarmankhan <work.armankhan@gmail.com>
2021-06-03 23:02:43 +0530

    :sparkles: checkout component UI

--- src/components/Cart/Checkout.tsx
+++ src/components/Cart/Checkout.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { ScrollView } from "react-native-gesture-handler";
 
-import { Box } from "../../theme/Theme";
+import { Box, Text } from "../../theme/Theme";
 import { useTheme } from "../../theme";
+import Button from "../UI/Button";
 
 import Card, { CardModel, CardTypes } from "./Card";
 import AddCard from "./AddCard";
@@ -16,6 +17,26 @@ const CARDS: CardModel[] = [
   { id: 1, type: CardTypes.MASTERCARD, last4Digits: 3456, expiration: "03/22" },
 ];
 
+interface LineItemProps {
+  label: string;
+  value: number;
+}
+
+const LineItem = ({ label, value }: LineItemProps) => (
+  <Box flexDirection="row" paddingVertical="s">
+    <Box flex={1}>
+      <Text variant="title3" color="background">
+        {label}
+      </Text>
+    </Box>
+    <Box>
+      <Text color="primary" variant="title3">
+        ${value}
+      </Text>
+    </Box>
+  </Box>
+);
+
 const Checkout: React.FC<CheckoutProps> = ({ minHeight }) => {
   const theme = useTheme();
   const [selectedCard, setSelectedCard] = useState(CARDS[0].id);
@@ -26,17 +47,49 @@ const Checkout: React.FC<CheckoutProps> = ({ minHeight }) => {
       style={{ paddingTop: minHeight + theme.spacing.m }}
     >
       <Box flex={1} padding="m">
-        <ScrollView horizontal>
-          <AddCard />
-          {CARDS.map((card) => (
-            <Card
-              key={card.id}
-              card={card}
-              selected={selectedCard === card.id}
-              onSelect={() => setSelectedCard(card.id)}
-            />
-          ))}
-        </ScrollView>
+        <Box>
+          <ScrollView showsHorizontalScrollIndicator={false} horizontal>
+            <AddCard />
+            {CARDS.map((card) => (
+              <Card
+                key={card.id}
+                card={card}
+                selected={selectedCard === card.id}
+                onSelect={() => setSelectedCard(card.id)}
+              />
+            ))}
+          </ScrollView>
+        </Box>
+        <Box marginTop="xl">
+          <Text color="background" variant="title3">
+            Delivery address
+          </Text>
+          <Box flexDirection="row" opacity={0.5} paddingVertical="m">
+            <Box flex={1}>
+              <Text color="background">23, Bakers st., tef av, new york</Text>
+            </Box>
+            <Box justifyContent="center" alignItems="center">
+              <Text color="background">Change</Text>
+            </Box>
+          </Box>
+
+          <LineItem label="Total Items (3)" value={189.94} />
+          <LineItem label="Standard Delivery" value={12} />
+          <LineItem label="Total Payment" value={201.84} />
+        </Box>
+
+        <Box
+          paddingVertical="l"
+          alignItems="center"
+          flex={1}
+          justifyContent="flex-end"
+        >
+          <Button
+            label="Swipe to pay $201.84"
+            onPress={() => true}
+            variant="primary"
+          />
+        </Box>
       </Box>
     </Box>
   );
